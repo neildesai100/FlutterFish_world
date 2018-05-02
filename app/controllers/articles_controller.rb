c8cf59f914782cb6452f45dd8104041275391afd
neildesai100 <neild@andrew.cmu.edu>
2018-05-02 13:04:10 -0400

    fixed controller for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   
   def index
     @articles = Article.alphabetical.paginate(:page => params[:page]).per_page(10)
@@ -21,7 +23,7 @@ class ArticlesController < ApplicationController
 
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article was successfully created.'
       redirect_to article_path(@article)
@@ -41,10 +43,17 @@ class ArticlesController < ApplicationController
     end
   end
 
-
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content, :category_id, :active)
   end
 end
