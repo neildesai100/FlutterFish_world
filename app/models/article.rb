4c3bad927c506f37f7d9b021d4f1c7b7cd90033f
neildesai100 <neild@andrew.cmu.edu>
2018-05-02 12:32:12 -0400

    added scopes, validations, relationships

---
+++ app/models/article.rb
@@ -0,0 +1,14 @@
+class Article < ApplicationRecord
+    belongs_to :category
+
+    #validations
+    validates_presence_of :title
+    validates_presence_of :content
+
+    #scopes
+    scope :alphabetical, -> { order('title') }
+    scope :active, -> {where('active = ?', true)}
+
+
+
+end
