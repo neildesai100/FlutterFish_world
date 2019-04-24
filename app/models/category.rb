a463c7a067a1d1be8fc658d146cf2d5224cea853
neildesai100 <34217559+neildesai100@users.noreply.github.com>
2019-04-24 15:49:37 -0400

    Added comment to category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,10 @@
 class Category < ApplicationRecord
+	
+	# I am making a comment here as my change
+	
 	has_many :photos
 
-	scope :active, 			 -> { where('active = ?', true) }
-  scope :alphabetical, -> { order('name') }
+	scope :active, -> { where('active = ?', true) }
+  	scope :alphabetical, -> { order('name') }
   
 end
